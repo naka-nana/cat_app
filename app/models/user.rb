9cecbbab7d721080e5c9d64e1e4465ff50ddab53
naka-nana <harunanapanda@icloud.com>
2025-01-21 21:36:20 +0900

    userモデル単体テスト実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :nickname, presence: true, length: { maximum: 6 }
+  validates :password, presence: true,
+                       format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'is invalid. Include both letters and numbers' }
 end
