b03844a63537bff8bda3d37e635cd975965c4807
naka-nana <harunanapanda@icloud.com>
2025-01-28 19:52:57 +0900

    診断のルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,9 @@ Rails.application.routes.draw do
       get 'mypage', to: 'cats#index', as: :mypage
     end
     resources :cats, only: [:new, :create, :show, :edit, :update, :destroy]
+    collection do
+      get 'diagnosis', to: 'diagnosis#new'    # 診断フォーム
+      post 'diagnosis', to: 'diagnosis#result' # 診断結果
+    end
   end
 end
