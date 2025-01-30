aad96bbfd7e845f23714d221d5e972ab42054adc
naka-nana <harunanapanda@icloud.com>
2025-01-30 12:33:45 +0900

    診断するねこ選択ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root "posts#index"
   get 'posts/index'
+
   resources :users, only: [] do
+    get 'diagnosis', to: 'diagnosis#select_cat', as: :select_diagnosis_cat
     member do
       get 'mypage', to: 'cats#index', as: :mypage
-    end
-    resources :cats, only: [:new, :create, :show, :edit, :update, :destroy]
-    collection do
-      get 'diagnosis', to: 'diagnosis#new'    # 診断フォーム
-      post 'diagnosis', to: 'diagnosis#result' # 診断結果
     end
   end
 end
