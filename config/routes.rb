e3809f97a00a20263560af136a10070c57ff9749
naka-nana <harunanapanda@icloud.com>
2025-02-02 14:08:34 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root "posts#index"
-  get 'posts/index'
+  resources :posts, only: [:new, :create, :index]
 
   resources :users, only: [] do
     get 'diagnosis', to: 'diagnosis#select_cat', as: :select_diagnosis_cat
