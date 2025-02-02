1f8bf84aa575e2c1bee0efd9e2b57e7e056dba91
naka-nana <harunanapanda@icloud.com>
2025-02-02 13:39:51 +0900

    マイグレーション

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_01_31_095411) do
+ActiveRecord::Schema[7.1].define(version: 2025_02_02_043735) do
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -48,6 +48,11 @@ ActiveRecord::Schema[7.1].define(version: 2025_01_31_095411) do
     t.datetime "updated_at", null: false
     t.string "diagnosis_result", collation: "utf8mb4_bin"
     t.index ["user_id"], name: "index_cats_on_user_id"
+  end
+
+  create_table "posts", charset: "utf8", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", charset: "utf8", force: :cascade do |t|
