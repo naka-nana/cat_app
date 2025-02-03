0de228d2364a6ecc78268de73c7b711f7903fe21
naka-nana <harunanapanda@icloud.com>
2025-02-03 16:42:43 +0900

    一覧画面表示

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user! # ログインしているユーザーのみが投稿可能
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_post, only: %i[show edit update destroy]
 
   def index
