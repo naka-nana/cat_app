f4a32780b2083122a8c77d53d1b392b733aaf1a7
naka-nana <harunanapanda@icloud.com>
2025-02-04 10:22:51 +0900

    モデルの関連付け

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -4,6 +4,8 @@ class Cat < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :age
   belongs_to :breed
+  has_many :post_cats
+  has_many :posts, through: :post_cats
   validates :name, presence: true, length: { maximum: 40 }
   validates :age_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :breed_id, numericality: { other_than: 1, message: "can't be blank" }
