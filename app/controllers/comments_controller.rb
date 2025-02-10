609a9bdc5390edcd536941dcd2259dbd5240722e
naka-nana <harunanapanda@icloud.com>
2025-02-10 11:17:26 +0900

    コントローラーでコメントの保存を制御

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,23 @@
 class CommentsController < ApplicationController
   def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.build(comment_params)
+    @comment.user = current_user
+
+    if @comment.save
+      respond_to do |format|
+        format.js # 非同期でcreate.js.erbを呼び出す
+      end
+    else
+      respond_to do |format|
+        format.js { render js: "alert('コメントの保存に失敗しました。');" }
+      end
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
   end
 end
