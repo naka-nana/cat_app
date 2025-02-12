3f20a9a471b399bcf56ea2ea08766446324b5a32
naka-nana <harunanapanda@icloud.com>
2025-02-12 11:34:52 +0900

    一覧ボタンのリンクを修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,12 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :ensure_correct_user, only: [:mypage]
+
+  def ensure_correct_user
+    return unless current_user != User.find(params[:id])
+
+    redirect_to root_path, alert: 'アクセス権がありません'
+  end
 
   def mypage
     @user = current_user
