cf205d325a7585a0a85d2592b2fc0455e2cc5e46
naka-nana <harunanapanda@icloud.com>
2025-02-12 12:09:42 +0900

    コントローラー作成

---
+++ app/controllers/relationships_controller.rb
@@ -0,0 +1,21 @@
+class RelationshipsController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    user = User.find(params[:following_id])
+    current_user.follow(user)
+    respond_to do |format|
+      format.html { redirect_to user }
+      format.turbo_stream
+    end
+  end
+
+  def destroy
+    user = Relationship.find(params[:id]).following
+    current_user.unfollow(user)
+    respond_to do |format|
+      format.html { redirect_to user }
+      format.turbo_stream
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'relationships/create'
+  get 'relationships/destroy'
   devise_for :users
   root "posts#index"
   resources :posts do
