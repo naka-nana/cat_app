35f270b7f9de49680b0cd46d31ef5c88cfe5b659
naka-nana <harunanapanda@icloud.com>
2025-02-16 13:39:31 +0900

    ユーザー詳細ページレイアウト

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,3 +4,12 @@ import "controllers"
 import Rails from "@rails/ujs";
 
 Rails.start();
+import { Application } from "@hotwired/stimulus";
+import { definitionsFromContext } from "@hotwired/stimulus-webpack-helpers";
+
+const application = Application.start();
+const context = require.context("./controllers", true, /\.js$/);
+application.load(definitionsFromContext(context));
+
+export { application };
+import "./controllers";
