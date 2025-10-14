805fe9baf9e52be2842f7b632e21324597035e10
naka-nana <harunanapanda@icloud.com>
2025-10-14 22:56:33 +0900

    いいねの結合テスト

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,10 +1,18 @@
 class LikesController < ApplicationController
+  # 任意：未ログインを弾く（posts と同じ挙動にそろえるなら推奨）
+  before_action :authenticate_user!
+
   before_action :set_post
 
   def create
+    # ① 自分の投稿なら 403（テストの期待に一致）
+    return forbid_own_post if @post.user_id == current_user.id
+
+    # ② 既にいいね済みなら既存処理
     return if already_liked?
 
-    @post.likes.create(user: current_user)
+    @post.likes.create!(user: current_user)
+
     respond_to do |format|
       format.html { redirect_to request.referer || posts_path }
       format.js
@@ -25,8 +33,10 @@ class LikesController < ApplicationController
       format.html { redirect_to request.referer || posts_path }
       format.js
       format.turbo_stream do
-        render turbo_stream: turbo_stream.replace("like_button_#{@post.id}", partial: 'posts/like_button',
-                                                                             locals: { post: @post })
+        render turbo_stream: [
+          turbo_stream.replace("like_button_#{@post.id}", partial: 'posts/like_button', locals: { post: @post }),
+          turbo_stream.replace("like_count_#{@post.id}", partial: 'posts/like_count', locals: { post: @post })
+        ]
       end
     end
   end
@@ -35,6 +45,16 @@ class LikesController < ApplicationController
 
   def set_post
     @post = Post.find(params[:post_id])
+  end
+
+  # ③ 自分の投稿をいいねしようとした時の共通レスポンス
+  def forbid_own_post
+    respond_to do |format|
+      # テストは :forbidden or redirect の両方許容だが、ここでは明示的に 403 を返す
+      format.html { head :forbidden }
+      format.js { head :forbidden }
+      format.turbo_stream { head :forbidden }
+    end
   end
 
   def already_liked?
